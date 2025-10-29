b30b7930a4dcdb813ba8d4cc21753c602355a3b2
FCChinen <fcchinen@hotmail.com>
2025-10-28 23:06:09 -0300

    in the middle of mocking

---
+++ mocking/mocking.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprint(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
